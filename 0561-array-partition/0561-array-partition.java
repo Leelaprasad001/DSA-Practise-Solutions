0221e03b73278f44dcdb062f15c90759cba61dfe
Leela Prasad Bantu <leelaprasad1607@gmail.com>
2024-07-25 12:13:51 +0530

    Time: 12 ms (96.38%), Space: 46.9 MB (12.01%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        for(int i = 0; i < nums.length; i += 2)
+            res += nums[i];
+        return res;
+    }
+}
