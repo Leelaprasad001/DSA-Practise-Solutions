0d2333bb000794235fbb6bb52fcdc980220edb97
Leela Prasad Bantu <leelaprasad1607@gmail.com>
2024-07-26 18:17:18 +0530

    Time: 3 ms (52.70%), Space: 45.7 MB (24.92%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxJump = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i > maxJump)
+                return false;
+            maxJump = Math.max(maxJump, i + nums[i]);
+        }
+        return true;
+    }
+}
