69898f91e33252233afed6d6f4dd69c0771accd8
Leela Prasad Bantu <leelaprasad1607@gmail.com>
2024-08-01 19:04:30 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (9.01%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -16,12 +16,12 @@
 class Solution {
     TreeNode prev = null;
     public void flatten(TreeNode root) {
-        {if(root == null)
+        if(root == null)
             return;
         flatten(root.right);
         flatten(root.left);
         root.right = prev;
         root.left = null;
-        prev = root;}
+        prev = root;
     }
 }
