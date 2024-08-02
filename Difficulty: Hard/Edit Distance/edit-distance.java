1a8fb2110307bffa11dbff7a9bed93d1b7f78e38
Leela Prasad Bantu <leelaprasad1607@gmail.com>
2024-08-02 10:51:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s1 = br.readLine();
+            String[] S = s1.split(" ");
+            String s = S[0];
+            String t = S[1];
+            Solution ob = new Solution();
+            int ans = ob.editDistance(s, t);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int editDistance(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        
+        int[][] dp = new int[m + 1][n + 1];
+     
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) {
+                    dp[i][j] = j; 
+                }
+                else if (j == 0) {
+                    dp[i][j] = i;
+                }
+                else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = 1 + Math.min(dp[i][j - 1], 
+                                    Math.min(dp[i - 1][j],
+                                             dp[i - 1][j - 1]));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
