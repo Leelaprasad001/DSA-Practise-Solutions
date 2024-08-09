fa592d6a95865299e837697dc7b6d9ebef4e2340
Leela Prasad Bantu <leelaprasad1607@gmail.com>
2024-08-09 12:30:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -5,34 +5,60 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-	public:
+public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+	vector<int> parent, size;
+	int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        vector<int> vis(V, 0);
-        //wt, node
-        pq.push({0,0});
-        int sum = 0;
-        while(!pq.empty()){
-            int node = pq.top().second;
-            int wt = pq.top().first;
-            pq.pop();
-            
-            if(vis[node] == 1)
-                continue;
-            vis[node] = 1;
-            sum += wt;
-            for(auto it:adj[node]){
-                int v = it[0];
-                int edwt = it[1];
-                if(!vis[v])
-                    pq.push({edwt, v});
+        vector<pair<int, pair<int, int>>> edges;
+        for (int i = 0; i < V; i++) {
+            for (auto it : adj[i]) {
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
+                edges.push_back({wt, {node, adjNode}});
             }
         }
         
-        return sum;
+        parent.resize(V + 1);
+        size.resize(V + 1);
+        for (int i = 0; i <= V; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+
+        sort(edges.begin(), edges.end());
+        int mstWt = 0;
+        for (auto it : edges) {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+
+            if (findUPar(u) != findUPar(v)) {
+                mstWt += wt;
+                unionBySize(u, v);
+            }
+        }
+        return mstWt;
     }
 };
 
