f36e0221524cc62f8e418b29d6674555950b3975
Leela Prasad Bantu <leelaprasad1607@gmail.com>
2024-10-17 13:43:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split Linked List Alternatingly/split-linked-list-alternatingly.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class Main {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = sc.nextLine();
+            Scanner ss = new Scanner(input);
+
+            while (ss.hasNextInt()) {
+                arr.add(ss.nextInt());
+            }
+
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+
+            Solution ob = new Solution();
+            Node[] result = ob.alternatingSplitList(head);
+            printList(result[0]);
+            printList(result[1]);
+        }
+
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+    // Function to append a new node with newData at the end of a linked list
+    Node[] alternatingSplitList(Node head) {
+        // code here
+        Node t1 = head;
+        Node first = head;
+        Node t2 = head.next;
+        Node sec = t2;
+        
+        while(t2 != null){
+            t1.next = t2.next;
+            t1 = t2;
+            t2 = t2.next;
+        }
+        
+        return new Node[]{first,sec};
+    }
+}
